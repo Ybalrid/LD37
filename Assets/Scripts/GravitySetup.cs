71b978b6a4819f3aa212b8c525175e63a51e16a9
Arthur Brainville <ybalrid@ybalrid.info>
2016-12-10 06:35:31 +0100

    add a test scene and add an object that control global gravity vector

---
+++ Assets/Scripts/GravitySetup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GravitySetup : MonoBehaviour
+{
+    public Vector3 gravity;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        Physics.gravity = gravity;
+    }
+}
