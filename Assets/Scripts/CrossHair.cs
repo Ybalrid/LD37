0d17597fd845839dfe2d31f20b4c860274ce916c
Arthur Brainville <ybalrid@ybalrid.info>
2016-12-11 04:46:07 +0100

    add crosshair and fix mouse lock code

---
+++ Assets/Scripts/CrossHair.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CrossHair : MonoBehaviour
+{
+    public Texture2D crosshairTexture;
+    private Rect position;
+
+    private int width, height;
+
+    // Use this for initialization
+    private void Start()
+    {
+        width = Screen.width;
+        height = Screen.height;
+        updatePosition();
+    }
+
+    private void FixedUpdate()
+    {
+        if (width != Screen.width || height != Screen.height) updatePosition();
+    }
+
+    private void updatePosition()
+    {
+        width = Screen.width;
+        height = Screen.height;
+        position = new Rect((width - crosshairTexture.width) / 2, (height - crosshairTexture.height) / 2, crosshairTexture.width, crosshairTexture.height);
+    }
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(position, crosshairTexture);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
--- Assets/Scripts/MouseLocking.cs
+++ Assets/Scripts/MouseLocking.cs
@@ -6,11 +6,11 @@ public class MouseLocking : MonoBehaviour
     // Use this for initialization
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
